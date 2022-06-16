46bc1b840277911cf488fdba11e13561bd628a41
Thanos Papapetrou <thanos.papapetrou@gmail.com>
2022-06-16 10:32:36 +0300

    Browsing files

---
+++ src/main/resources/static/js/browse.js
@@ -0,0 +1,24 @@
+function browse() {
+    const request = new XMLHttpRequest();
+    request.onload = function() {
+        const files = JSON.parse(this.response).files;
+        if (files.length) {
+            const list = document.createElement('ul');
+            files.forEach(function (file) {
+                const element = document.createElement('li');
+                const link = document.createElement('a');
+                link.href = '/edit.html?file=' + file;
+                link.appendChild(document.createTextNode(file));
+                element.appendChild(link);
+                list.appendChild(element);
+            });
+            document.body.appendChild(list);
+        } else {
+            const noFiles = document.createElement('p');
+            noFiles.appendChild(document.createTextNode('No files found'));
+            document.body.appendChild(noFiles);
+        }
+    }
+    request.open("GET", "/files", true);
+    request.send();
+}
