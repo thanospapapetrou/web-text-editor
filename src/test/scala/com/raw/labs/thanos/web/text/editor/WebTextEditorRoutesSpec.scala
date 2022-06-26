5637dc571118cf9a4f07bf1807cab9387d7f22d4
Thanos Papapetrou <thanos.papapetrou@gmail.com>
2022-06-26 19:28:06 +0300

    Fixed tests

--- src/test/scala/com/raw/labs/thanos/web/text/editor/WebTextEditorRoutesSpec.scala
+++ src/test/scala/com/raw/labs/thanos/web/text/editor/WebTextEditorRoutesSpec.scala
@@ -1,7 +1,5 @@
 package com.raw.labs.thanos.web.text.editor
 
-//#user-routes-spec
-//#test-top
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model._
@@ -10,86 +8,48 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-//#set-up
 class WebTextEditorRoutesSpec extends AnyWordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
-  //#test-top
-
-  // the Akka HTTP route testkit does not yet support a typed actor system (https://github.com/akka/akka-http/issues/2036)
-  // so we have to adapt for now
   lazy val testKit = ActorTestKit()
+
   implicit def typedSystem = testKit.system
+
   override def createActorSystem(): akka.actor.ActorSystem =
     testKit.system.classicSystem
 
-  // Here we need to implement all the abstract members of UserRoutes.
-  // We use the real UserRegistryActor to test it while we hit the Routes,
-  // but we could "mock" it by implementing it in-place or by using a TestProbe
-  // created with testKit.createTestProbe()
   val userRegistry = testKit.spawn(FileRegistry())
   lazy val routes = new WebTextEditorRoutes(userRegistry).routes
 
-  // use the json formats to marshal and unmarshall objects in the test
-  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import JsonFormats._
-  //#set-up
+  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
-  //#actual-test
-  "UserRoutes" should {
-    "return no users if no present (GET /users)" in {
-      // note that there's no need for the host part in the uri:
-      val request = HttpRequest(uri = "/users")
-
+  "WebTextEditorRoutes" should {
+    "return no files if no present (GET /files)" in {
+      val request = HttpRequest(uri = "/files")
       request ~> routes ~> check {
         status should ===(StatusCodes.OK)
-
-        // we expect the response to be json:
         contentType should ===(ContentTypes.`application/json`)
-
-        // and no entries should be in the list:
-        entityAs[String] should ===("""{"users":[]}""")
+        entityAs[String] should ===("""{"files":[]}""")
       }
     }
-    //#actual-test
 
-    //#testing-post
-    "be able to add users (POST /users)" in {
-      val user = File("Kapi", 42, "jp")
-      val userEntity = Marshal(user).to[MessageEntity].futureValue // futureValue is from ScalaFutures
-
-      // using the RequestBuilding DSL:
-      val request = Post("/users").withEntity(userEntity)
-
+    "be able to create files (POST /files)" in {
+      val name = "foo"
+      val request = Post(s"/files/$name")
       request ~> routes ~> check {
         status should ===(StatusCodes.Created)
-
-        // we expect the response to be json:
         contentType should ===(ContentTypes.`application/json`)
-
-        // and we know what message we're expecting back:
-        entityAs[String] should ===("""{"description":"User Kapi created."}""")
+        entityAs[String] should ===("""{"description":"File foo created."}""")
       }
     }
-    //#testing-post
 
-    "be able to remove users (DELETE /users)" in {
-      // user the RequestBuilding DSL provided by ScalatestRouteSpec:
-      val request = Delete(uri = "/users/Kapi")
+    "be able to remove files (DELETE /files)" in {
+      val request = Delete(uri = "/files/foo")
 
       request ~> routes ~> check {
         status should ===(StatusCodes.OK)
-
-        // we expect the response to be json:
         contentType should ===(ContentTypes.`application/json`)
-
-        // and no entries should be in the list:
-        entityAs[String] should ===("""{"description":"User Kapi deleted."}""")
+        entityAs[String] should ===("""{"description":"File foo deleted."}""")
       }
     }
-    //#actual-test
   }
-  //#actual-test
-
-  //#set-up
 }
-//#set-up
-//#user-routes-spec
